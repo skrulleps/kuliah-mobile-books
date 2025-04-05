1d6e0e1180fd692fa67f1089fe5d6e5600d75f99
Mochamed Fadhlan Tuhairi <fadhlantuhairi@gmail.com>
2025-04-05 14:17:13 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/global/books/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.global.books
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Books Fadhlan',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const FuturePage(),
+    );
+  }
+}
+
+class FuturePage extends StatefulWidget {
+  const FuturePage({super.key});
+
+  @override
+  State<FuturePage> createState() => _FuturePageState();
+}
+
+class _FuturePageState extends State<FuturePage> {
+  String result = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Future Page'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Spacer(),
+              ElevatedButton(onPressed: () {}, child: const Text('Go')),
+              const Spacer(),
+              Text(result),
+              const CircularProgressIndicator(),
+              const Spacer(),
+            ],
+          ),
+        ));
+  }
+
+  Future<http.Response> getData() async {
+    const authority = 'www.googleapis.com';
+    const path = '.books/v1/volumes/oFUEAAAAMBAJ';
+    Uri url = Uri.https(authority, path);
+    return http.get(url);
+  }
+}
